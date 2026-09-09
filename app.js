db1b08f6b187a6d3b9fc7f94b9362dd377300245
PabliVM <pablo.vegamartin@gmail.com>
2026-09-09 19:57:40 +0200

    Adjust row height and fix flag condition

--- app.js
+++ app.js
@@ -2174,7 +2174,7 @@ function renderCalVertical(events,season){
   var dowLetters=["L","M","X","J","V","S","D"];
   var months=[];
   for(var m=6;m<18;m++){var y=startYear+(m>=12?1:0);var mo=m%12;months.push({year:y,month:mo,label:monthNames[m-6]});}
-  var ROWH=20;
+  var ROWH=34;
   var LANEW=21;
   var pad2=function(n){return String(n).padStart(2,"0");};
   var global=computeGlobalLanes(events);
